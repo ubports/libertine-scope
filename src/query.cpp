3c6d122f3c22992738f244430edb74f3f80b1828
Stephen M. Webb <stephen.webb@canonical.com>
2016-01-06 11:01:18 -0500

    src/query.cpp: added category renderer

--- src/query.cpp
+++ src/query.cpp
@@ -14,18 +14,53 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include <query.h>
-#include <localization.h>
 
 #include "applauncher.h"
 #include "container.h"
 #include "libertine.h"
-#include <unity/scopes/Annotation.h>
 #include <unity/scopes/CategorisedResult.h>
 #include <unity/scopes/CategoryRenderer.h>
 #include <unity/scopes/QueryBase.h>
 #include <unity/scopes/SearchReply.h>
 
 namespace usc = unity::scopes;
+
+
+namespace
+{
+
+/**
+ * A custom rendering layout brazenly stolen from the click scope, so they look
+ * sorta similar.  At least until they change theirs.
+ */
+std::string const CATEGORY_APPS_DISPLAY = R"(
+    {
+        "schema-version" : 1,
+        "template" : {
+            "category-layout" : "grid",
+            "card-size": "small"
+        },
+        "components" : {
+            "title" : "title",
+            "art" : {
+                "field": "art",
+                "fill-mode": "fit"
+            }
+        }
+    }
+)";
+
+
+/**
+ * Generates a ubuntu-application-launcher URI for a contained desktop file.
+ */
+static std::string
+app_uri(Container const& container, AppLauncher const& app)
+{
+  return "appid://" + container.id() + "/" + app.id() + "/0.0";
+}
+
+} // anonymous namespace
 
 
 Query::
@@ -42,16 +77,6 @@ cancelled()
 }
 
 
-/**
- * Generates a ubuntu-application-launcher URI for a contained desktop file.
- */
-static std::string
-app_uri(Container const& container, AppLauncher const& app)
-{
-  return "appid://" + container.id() + "/" + app.id() + "/0.0";
-}
-
-
 void Query::
 run(usc::SearchReplyProxy const& reply)
 {
@@ -61,7 +86,11 @@ run(usc::SearchReplyProxy const& reply)
   Libertine libertine;
   for (auto const& container: libertine.get_container_list())
   {
-    auto category = reply->register_category(container->id(), container->name(), "icon");
+    auto category = reply->register_category(container->id(),
+                                             container->name(),
+                                             "Application",
+                                             usc::CategoryRenderer(CATEGORY_APPS_DISPLAY));
+
     for (auto const& app: container->app_launchers())
     {
       if (app.no_display())
@@ -69,7 +98,7 @@ run(usc::SearchReplyProxy const& reply)
 
       usc::CategorisedResult result(category);
       result.set_title(app.name());
-      result.set_art("file://" + app.icon());
+      result.set_art(app.icon());
       result.set_uri(app_uri(*container, app));
       if (!reply->push(result))
       {
